e8a6add867136026723b0cb21781f4c0095705d4
av1401 <rsabhi1401@gmail.com>
2025-02-25 14:38:32 +0530

    [Abhishek] use of rest controller with path variable

--- src/main/java/com/springintroduction/SpringIntro/controllers/SpringContollers.java
+++ src/main/java/com/springintroduction/SpringIntro/controllers/SpringContollers.java
@@ -1,6 +1,7 @@
 package com.springintroduction.SpringIntro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +17,14 @@ public class SpringContollers {
     public String sayHello(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz";
     }
+
+    //using  pathvariable
+
+    @GetMapping("/hello/param/{name}")
+    //http://localhost:8080/hello/param/Mark
+    public String sayHello2(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    
 }
