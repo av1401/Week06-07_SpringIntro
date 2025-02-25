d5e2953151d5f1ba6a37348356d5ef8823641bca
av1401 <rsabhi1401@gmail.com>
2025-02-25 14:05:19 +0530

    [Abhishek] use of rest controller with Request Param

--- src/main/java/com/springintroduction/SpringIntro/controllers/SpringContollers.java
+++ src/main/java/com/springintroduction/SpringIntro/controllers/SpringContollers.java
@@ -1,6 +1,7 @@
 package com.springintroduction.SpringIntro.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,11 @@ public class SpringContollers {
     @GetMapping("/hello-w")
     public String hello(){
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/hello/query")
+    // http://localhost:8080/hello/query?name=Mark
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
